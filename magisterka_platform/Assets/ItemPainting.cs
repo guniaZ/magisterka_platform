f80a555daeb632912b5e9ac73d9f6632b217c7af
guniaZ <gunia.zuzanna@gmail.com>
2022-07-05 12:37:02 +0200

    Added: Trigger (on) style transfer

---
+++ magisterka_platform/Assets/ItemPainting.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ItemPainting : TriggerEvent
+{
+    public GameObject collectedFX;
+    public AudioClip sound;
+    bool isUsed = false;
+
+    public override void OnContactPlayer()
+    {
+        if (isUsed)
+            return;
+
+        isUsed = true;
+        
+
+
+        SoundManager.PlaySfx(sound);
+        Destroy(gameObject);
+    }
+}
---
+++ magisterka_platform/Assets/StTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Barracuda;
+
+public class StTrigger : MonoBehaviour
+{
+    public GameObject camera;
+    public NNModel modelAsset;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        camera.GetComponent<StyleTransfer>().modelAsset = modelAsset;
+        camera.GetComponent<StyleTransfer>().enabled = true;
+    }
+}
