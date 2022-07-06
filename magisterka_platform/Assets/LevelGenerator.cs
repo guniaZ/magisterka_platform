62beaf77247e59ff23bc57bbd09e4bc334fdbc61
guniaZ <gunia.zuzanna@gmail.com>
2022-07-06 12:16:59 +0200

    Basic Lvl2

---
+++ magisterka_platform/Assets/LevelGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    [SerializeField] private Transform levelPart_1;
+
+    private void Awake()
+    {
+        Instantiate(levelPart_1, new Vector4(73, 15, 0), Quaternion.identity);
+    }
+}
