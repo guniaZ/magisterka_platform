e0577685af7a1bb94cbd55cf54447e08e73d669d
guniaZ <gunia.zuzanna@gmail.com>
2022-07-07 11:25:52 +0200

    Lvl2: basic spawn

--- magisterka_platform/Assets/LevelGenerator.cs
+++ magisterka_platform/Assets/LevelGenerator.cs
@@ -4,10 +4,36 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
+    [SerializeField] private Transform levelPart_Start;
     [SerializeField] private Transform levelPart_1;
+
+    private Vector4 lastEndPosition;
 
     private void Awake()
     {
-        Instantiate(levelPart_1, new Vector4(73, 15, 0), Quaternion.identity);
+        /*
+        lastEndPosition = levelPart_Start.Find("EndPosition").position;
+        SpawnLevelPart();
+        */
+        
+        Transform lastLevelPartTransform;
+        lastLevelPartTransform = SpawnLevelPart(levelPart_Start.Find("EndPosition").position);
+        lastLevelPartTransform = SpawnLevelPart(lastLevelPartTransform.Find("EndPosition").position);
+        lastLevelPartTransform = SpawnLevelPart(lastLevelPartTransform.Find("EndPosition").position);
+        lastLevelPartTransform = SpawnLevelPart(lastLevelPartTransform.Find("EndPosition").position);
+        
+
+    }
+
+    private void SpawnLevelPart()
+    {
+        Transform lastLevelPartTransform = SpawnLevelPart(lastEndPosition);
+        lastEndPosition = lastLevelPartTransform.Find("EndPosition").position;
+    }
+
+    private Transform SpawnLevelPart(Vector3 spawnPosition)
+    {
+        Transform levelPartTransform = Instantiate(levelPart_1, spawnPosition, Quaternion.identity);
+        return levelPartTransform;
     }
 }
